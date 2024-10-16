736d01774d36a9420eb9a74de3dc3a9e9817421a
Ahill Pranav M <itisahill.m@gmail.com>
2024-10-16 09:30:08 +0530

    This program uses strcspn for null terminationa and checks only the first half of the string which manages time complexity

---
+++ Palindrome.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    int flag=1;
+    printf("Enter the input:");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str,"\n")]='\0';
+    int start=0, end=strlen(str)-1;
+    while(start<=end)
+    {
+        if(str[start]!=str[end])
+        {
+            flag=0;
+            break;
+        }
+        start++,end--;
+    }
+    if (flag)
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("Not a palindrome");
+    }
+}
