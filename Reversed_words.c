3bd09fed4e078f3992a16b325d0ebe3d7025652e
Ahill Pranav M <itisahill.m@gmail.com>
2024-10-23 10:02:42 +0530

    This code prints the words in a string in reverse order

--- Reversed_words.c
+++ Reversed_words.c
@@ -6,4 +6,20 @@ int main()
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
     str[strcspn(str,"\n")]= 0;
+    int end = strlen(str);
+    for (int start=strlen(str)-1; start >=0;start-- )
+    {
+        if(str[start]==' ' || start==0)
+        {
+             int wordStart = (start == 0) ? start : start + 1;
+            for (int i = wordStart; i <=end; i++)
+            {
+                printf("%c",str[i]);
+            }
+            end=start;
+            
+            printf(" ");
+        }
+    }
+    
 }
