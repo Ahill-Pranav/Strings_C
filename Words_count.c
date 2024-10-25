b64397ebd14d2377cd409ac5ca79451deb369e7a
Ahill Pranav M <itisahill.m@gmail.com>
2024-10-25 09:24:06 +0530

    Added null line termination

--- Words_count.c
+++ Words_count.c
@@ -6,6 +6,7 @@ int main()
     char str[100];
     printf("Enter the string:");
     fgets(str,sizeof(str),stdin);
+    str[strcspn(str,"\n")]=0;
     for(int i=0;str[i]!='\0';i++)
     {
         if (str[i]==' ')
