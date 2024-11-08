7df5fe2cb2e318a974726dd6800d1e7f4eb7811c
Ahill Pranav M <152412437+Ahill-Pranav@users.noreply.github.com>
2024-11-08 10:31:47 +0530

    Update Reversed_words.c
    This code uses tokenization to reverse the words

--- Reversed_words.c
+++ Reversed_words.c
@@ -1,25 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<string.h>
-int main()
+int main() 
 {
-    char str[100];
-    printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin);
-    str[strcspn(str,"\n")]= 0;
-    int end = strlen(str);
-    for (int start=strlen(str)-1; start >=0;start-- )
-    {
-        if(str[start]==' ' || start==0)
-        {
-             int wordStart = (start == 0) ? start : start + 1;
-            for (int i = wordStart; i <=end; i++)
-            {
-                printf("%c",str[i]);
-            }
-            end=start;
-            
-            printf(" ");
-        }
+
+char *words[50], str[100],temp[100];
+int count=0;
+fgets(str,sizeof(str),stdin);
+str[strcspn(str,"\n")]=0;
+strcpy(temp, str);
+char *token=strtok(temp," ");
+while(token!=NULL)
+{
+    words[count++]=token;
+    token=strtok(NULL," ");
+}
+for (int i = count - 1; i >= 0; i--) {
+        printf("%s", words[i]);
+        if (i > 0) printf(" "); // Add space between words
     }
-    
+    printf("\n");
 }
